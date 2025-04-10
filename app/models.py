c3c67bc72f2f022fe3cedc6116e8749038e8f78b
dwijk <dwijkarmaleen@gmail.com>
2025-04-10 10:59:39 +0530

    new db

--- app/models.py
+++ app/models.py
@@ -5,13 +5,7 @@ import uuid
 class DataStore(models.Model):
     name = models.CharField(max_length=255)
     data = models.JSONField()
-
-
-
-class LeadgenData(models.Model):
-    lead_id = models.CharField(max_length=50, unique=True)
-    user_uuid = models.ForeignKey('UserData', on_delete=models.CASCADE, to_field='uuid')
-    lead_data = models.JSONField() 
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class UserData(models.Model):
@@ -21,9 +15,16 @@ class UserData(models.Model):
     
     def __str__(self):
         return f"UserData UUID: {self.uuid}"
+    
+class LeadgenData(models.Model):
+    lead_id = models.CharField(max_length=50, unique=True)
+    user_uuid = models.ForeignKey(UserData, on_delete=models.CASCADE)
+    lead_data = models.JSONField() 
+
+
 
 class TokenDate(models.Model):
-    user_uuid = models.ForeignKey('UserData', on_delete=models.CASCADE, to_field='uuid')
+    user_uuid = models.ForeignKey(UserData, on_delete=models.CASCADE)
     short_time_access_token = models.CharField(max_length=512, unique=True)
     long_time_access_token = models.CharField(max_length=512, unique=True)
     short_token_created_date = models.DateField()
